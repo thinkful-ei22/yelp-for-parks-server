39c3a66fd663aa8d0ff6ca72781432a3a1307b4e
Daniyar Salikhov <salihovdr@gmail.com>
2018-09-24 16:22:02 -0400

    Create UserSchema and router

--- config.js
+++ config.js
@@ -3,6 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://localhost/yelp-for-parks-backend',
   TEST_DATABASE_URL:
---
+++ routers/user.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password', 'firstName', 'lastName'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if(missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if(nonStringField){
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if(nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields
+
+});
