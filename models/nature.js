de6f473c422f2e1df28efd130992dae11d279424
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-25 07:28:02 -0700

    Created ref of User in nature.js

---
+++ models/nature.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const NatureSchema = mongoose.Schema({
+  title: {type: String, required: true, unique: true},
+  address: {type: String, required: true, unique: true},
+  city: {type: String, required: true},
+  state: {type: String, required: true},
+  zipCode: {type: Number, required: true},
+  description: {type: String, required: true},
+  amenities: {type: Array},
+  specialInstructions: {type: String},
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+const Nature = mongoose.model('Nature', NatureSchema);
+
+module.exports = Nature;
