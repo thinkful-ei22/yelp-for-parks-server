802d0a8c4855450a11b1153efe6ad0184c5b868e
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-25 20:05:50 -0700

    Created put and delete endpoints - delete POSTMAN test passed - getting a 500 on put

--- routers/locations.js
+++ routers/locations.js
@@ -20,7 +20,7 @@ router.get('/', (req, res, next) => {
 //Get location by Id
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  
+
   Location.findById(id)
     .then(location => {
       if(location){
@@ -105,5 +105,97 @@ router.post('/', passport.authenticate('jwt', { session: false, failWithError: t
       next(err);
     });
 });
+
+router.put('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), (req, res, next) => {
+  const { id } = req.params;
+
+  const {
+    title,
+    address,
+    city,
+    state,
+    zipCode,
+    description,
+    amenities,
+    specialInstructions
+  } = req.body;
+
+  const updatedLocation = {
+    title,
+    address,
+    city,
+    state,
+    zipCode,
+    description,
+    amenities,
+    specialInstructions
+  };
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (title === '') {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (address === '') {
+    const err = new Error('Missing `address` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (city === '') {
+    const err = new Error('Missing `city` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (state === '') {
+    const err = new Error('Missing `state` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (zipCode === '') {
+    const err = new Error('Missing `zipCode` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (description === '') {
+    const err = new Error('Missing `description` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  Location.findOneAndUpdate({ _id: id }, updatedLocation, { new: true })
+    .then(updatedLocation => {
+      if(updatedLocation) {
+        res.json(updatedLocation);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    })
+});
+
+router.delete('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), (req, res, next) => {
+  const { id } = req.params;
+
+  Location.findOneAndRemove({ _id: id })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      next(err);
+    });
+})
 
 module.exports = router;
