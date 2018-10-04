733d110abeac8a1685d5c061e5e2cd8af93b77a4
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-04 13:47:23 -0700

    Changed the res betweens lines 28 to 30 to response. There were 2 res references

--- routers/locationmap.js
+++ routers/locationmap.js
@@ -9,8 +9,7 @@ const axios = require('axios');
 
 const router = express.Router();
 
-//POST/Create comment
-router.post('/', passport.authenticate('jwt', {session: false, failWithError: true}), (req, res, next) => {
+router.post('/', passport.authenticate('jwt', { session: false, failWithError: true }), (req, res, next) => {
   const { adminDistrict, postalCode, locality, addressLine } = req.body;
 
   const newLocationMap = {
@@ -21,22 +20,21 @@ router.post('/', passport.authenticate('jwt', {session: false, failWithError: tr
     key: config.key
   };
 
+  console.log('We are right before the axios request!')
   let latitude;
   let longitude;
   let coordinates;
-  axios.get(`http://dev.virtualearth.net/REST/v1/Locations/US/${newLocationMap.adminDistrict}/${newLocationMap.postalCode}/${newLocationMap.locality}/${newLocationMap.addressLine}?o=json&key=${newLocationMap.key}`)
-  .then(res => {
-    console.log("Latitude within axios", res.data.resourceSets[0].resources[0].point.coordinates[0])
-    console.log("Longitude within axios", res.data.resourceSets[0].resources[0].point.coordinates[1])
-    latitude = res.data.resourceSets[0].resources[0].point.coordinates[0];
-    longitude = res.data.resourceSets[0].resources[0].point.coordinates[1];
+  axios(`http://dev.virtualearth.net/REST/v1/Locations/US/${newLocationMap.adminDistrict}/${newLocationMap.postalCode}/${newLocationMap.locality}/${newLocationMap.addressLine}?o=json&key=${newLocationMap.key}`)
+  .then(response => {
+    latitude = response.data.resourceSets[0].resources[0].point.coordinates[0];
+    longitude = response.data.resourceSets[0].resources[0].point.coordinates[1];
     coordinates = {
       lat: latitude,
       lng: longitude
     }
-    console.log("Right before coordinates return statement in axios", coordinates);
     return res.json(coordinates);
   })
+  .catch(err => console.log(err))
 });
 
 module.exports = router;
