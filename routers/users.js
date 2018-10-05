e28e07b155ca793819237573fb40f6cef3b1e192
Daniyar Salikhov <salihovdr@gmail.com>
2018-10-05 14:34:29 -0400

    added user edit endpoint

--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const User = require('../models/users');
 
@@ -159,6 +160,129 @@ router.post('/', (req, res) => {
     })
     .then(user => {
       return res.status(201).json(user);
+    })
+    .catch(err => {
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({ code: 500, message: 'Internal server error' });
+    });
+});
+
+
+//EDIT user
+router.put('/:id', passport.authenticate('jwt', { session: false, failWithError: true }), (req, res, next) => {
+
+  const { id } = req.params;
+
+  const requiredFields = ['username', 'password', 'firstName', 'lastName'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 10,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let { username, password, firstName, lastName } = req.body;
+
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+
+  return User.find({ username })
+    .countDocuments()
+    .then(count => {
+      if (count > 0) {
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.findByIdAndUpdate(id, {
+        username,
+        password: hash,
+        firstName,
+        lastName
+      }, { new: true });
+    })
+    .then(updatedUser => {
+      if(updatedUser){
+        res.json(updatedUser)
+      } else {
+        next();
+      }
     })
     .catch(err => {
       if (err.reason === 'ValidationError') {
